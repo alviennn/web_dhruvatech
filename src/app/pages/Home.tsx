453c11c60d9b15a2135a394b2b357cd1f5c7d2ad
Jantopthegreat <fauzangriya@gmail.com>
2026-06-28 22:37:31 +0700

    fix: add HelmetProvider and meta tags for improved SEO on Home page

--- src/app/pages/Home.tsx
+++ src/app/pages/Home.tsx
@@ -6,8 +6,16 @@ import {
   HomeFAQ,
 } from "../components/Sections";
 import { Hero } from "../components/Hero";
+import { Helmet, HelmetProvider } from "react-helmet-async";
 
 export function Home() {
+  <HelmetProvider>
+    <Helmet>
+      <title>Dhurva Tech - Home</title>
+      <meta name="description" content="Dhurva Tech Home Page" />
+    </Helmet>
+  </HelmetProvider>;
+
   return (
     <>
       <Hero />
