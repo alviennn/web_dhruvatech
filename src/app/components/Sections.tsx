7ca3c0eda38fd5df294f632e0ae4a44d6f745d48
Jantopthegreat <fauzangriya@gmail.com>
2026-06-29 21:08:20 +0700

    fix: update logo import path and add new logo image for navbar

--- src/app/components/Sections.tsx
+++ src/app/components/Sections.tsx
@@ -20,7 +20,7 @@ import {
 } from "lucide-react";
 import { SectionHeader } from "./shared";
 import { useT } from "../providers";
-import logo from "../../imports/8.png";
+import logo from "../../imports/logonavbar.png";
 import ctaImage from "../../imports/cta.png";
 import ctaLogoMark from "../../imports/dhruva-mark.png";
 import why from "../../imports/why.png";
