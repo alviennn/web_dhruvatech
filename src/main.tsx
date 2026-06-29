685f6988f658798888f516417076dff53abd4106
alviennn <alvien.rnp@gmail.com>
2026-06-29 22:08:59 +0700

    feat: add Poppins font and update styles to use fontsource package

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,11 @@
 import { createRoot } from "react-dom/client";
-import App from "./app/App.tsx";
+import App from "./app/App";
 import "./styles/index.css";
 import { HelmetProvider } from "react-helmet-async";
+import "@fontsource/poppins/400.css";
+import "@fontsource/poppins/500.css";
+import "@fontsource/poppins/600.css";
+import "@fontsource/poppins/700.css";
 
 createRoot(document.getElementById("root")!).render(<App />);
 <HelmetProvider>
