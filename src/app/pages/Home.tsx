bb336019fb9f8fa97c6d0326363071ce1f20604c
Jantopthegreat <fauzangriya@gmail.com>
2026-06-29 21:37:14 +0700

    fix: update SEO meta tags and title in Home component

--- src/app/pages/Home.tsx
+++ src/app/pages/Home.tsx
@@ -11,8 +11,13 @@ import { Helmet, HelmetProvider } from "react-helmet-async";
 export function Home() {
   <HelmetProvider>
     <Helmet>
-      <title>Dhurva Tech - Home</title>
-      <meta name="description" content="Dhurva Tech Home Page" />
+      <title>
+        Dhurva Tech | Software House Yogyakarta - Web, Mobile & AI Development
+      </title>
+      <meta
+        name="description"
+        content="Dhurva Tech membantu membangun produk digital anda mulai dari website company profile, aplikasi mobile, hingga solusi berbasis AI/ML. Mari Realisasikan Ide Besar Anda Bersama Kami!"
+      />
     </Helmet>
   </HelmetProvider>;
 
