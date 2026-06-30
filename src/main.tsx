aa05dc4e137a16abdaf09fdb3a8baa57fae3046d
alviennn <alvien.rnp@gmail.com>
2026-06-30 17:22:02 +0700

    refactor: reorganize HelmetProvider usage in main.tsx for improved readability

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,15 @@
 import { createRoot } from "react-dom/client";
+import { HelmetProvider } from "react-helmet-async";
 import App from "./app/App";
 import "./styles/index.css";
-import { HelmetProvider } from "react-helmet-async";
+
 import "@fontsource/poppins/400.css";
 import "@fontsource/poppins/500.css";
 import "@fontsource/poppins/600.css";
 import "@fontsource/poppins/700.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
-<HelmetProvider>
-  <App />
-</HelmetProvider>;
+createRoot(document.getElementById("root")!).render(
+  <HelmetProvider>
+    <App />
+  </HelmetProvider>,
+);
